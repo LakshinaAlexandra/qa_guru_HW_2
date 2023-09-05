0f31db24f7b04a1c334945c368ac3d11f8192a58
LakshinaAlexandra <32337786+LakshinaAlexandra@users.noreply.github.com>
2023-09-05 23:37:41 +0300

    message was changed - change master

--- src/main/java/aleksandra/lakshina/Main.java
+++ src/main/java/aleksandra/lakshina/Main.java
@@ -2,6 +2,6 @@ package aleksandra.lakshina;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! change master");
     }
 }
